e3f5e549d485cd051c794d2f4f4daf9d102688ff
Aminagued <ayouya.gued@gmail.com>
2024-07-29 13:44:19 -0700

    first comment

---
+++ src/data.js
@@ -0,0 +1,66 @@
+const reviews = [
+  {
+    id: 1,
+    name: "Stive john",
+    job: "Web Developer",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzHQv_th9wq3ivQ1CVk7UZRxhbPq64oQrg5Q&usqp=CAU",
+    text: "G2 Seller Solutions in an online review website and intent data provider. The vendor promises to help tech companies establish their presence, build their online reputation, and connect with active buyers in their target audience.",
+  },
+  {
+    id: 2,
+    name: "Chaima Gued",
+    job: "Drug Designer",
+    image:
+      "https://images.pexels.com/photos/2726111/pexels-photo-2726111.jpeg?cs=srgb&dl=pexels-masha-raymers-2726111.jpg&fm=jpg",
+    text: "Gartner Peer Insights is a technology review platform where users can review software they have experience with as well as view reviews from other users. These reviews are used to create interactive visualizations with customizable criteria.",
+  },
+  {
+    id: 3,
+    name: "Maria Maria Diego",
+    job: "Software engineer",
+    image:
+      "https://expertphotography.b-cdn.net/wp-content/uploads/2018/10/mike-fox-467499-unsplash.jpg",
+    text: "Peerspot (formerly IT Central Station) is a technology review platform for enterprise technology. Peerspot sources reviews from product users authenticated by a three-step process. Vendors use Peerspot to connect with potential buyers and communicate with reviewers.",
+  },
+  {
+    id: 4,
+    name: "Edward Roawn",
+    job: "Computer System analyst",
+    image:
+      "https://www.phdmedia.com/romania/wp-content/uploads/sites/73/2015/05/temp-people-profile.jpg",
+    text: "The list of products below is based purely on reviews (sorted from most to least). There is no paid placement and analyst opinions do not influence their rankings. Here is our Promise to Buyers to ensure information on our site is reliable, useful, and worthy of your trust.",
+  },
+  {
+    id: 5,
+    name: "Erika betalya",
+    job: "Information Security Manager",
+    image: "https://photof8.com/wp-content/uploads/2016/04/Di_Watson27-3.jpg",
+    text: "Openbase is a community platform for developers, that enables them to discover, compare and choose open-source technologies that fit their needs. Openbase offers reviews, metrics, and insights accessed by millions of developers worldwide.",
+  },
+  {
+    id: 6,
+    name: "Amina Gued",
+    job: "Data Scientist",
+    image:
+      "https://funkylife.in/wp-content/uploads/2022/04/Instagram-dp-280-1.jpg",
+    text: "Software Advice is an online service for helping buyers select technology for their business. They provide personalized suggestions based on user reviews as well as insight from industry researchers. Software Advice can also provide leads to vendors.",
+  },
+  {
+    id: 7,
+    name: "Sevile sillya",
+    job: "Software developer",
+    image: "https://photof8.com/wp-content/uploads/2017/01/WSS500.jpg",
+    text: "TestBox helps users test and compare multiple software solutions so they can choose the best one for a team or situation. The solution aims to take the pain out of finding the right software for a company with access to live products that users can play with side-by-side",
+  },
+  {
+    id: 8,
+    name: "Roma lisa",
+    job: "Product Manager",
+    image:
+      "https://img.freepik.com/free-photo/happiness-wellbeing-confidence-concept-cheerful-attractive-african-american-woman-curly-haircut-cross-arms-chest-self-assured-powerful-pose-smiling-determined-wear-yellow-sweater_176420-35063.jpg?w=2000",
+    text: "GetApp is an online resource for buyers to research business technologies. Users can view reviews from other buyers and create side-by-side comparisons to help choose between products.",
+  },
+];
+
+export default reviews;
